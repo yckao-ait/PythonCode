73013841dc90cad40d63e810d1671b13a993861a
yckao-ait <42824778+yckao-ait@users.noreply.github.com>
2018-09-11 11:34:44 +0800

    Update face & eye detection samples

--- opencv_demo05.py
+++ opencv_demo05.py
@@ -1,6 +1,6 @@
 #! Python 3 
-# opencv_demo04_facedet.py
-# 09/10/2018 sample created
+# opencv_demo05_face_eye_det.py
+# 09/11/2018 sample created
 #--------------------------
 import os
 import cv2
@@ -16,22 +16,29 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 os.chdir(basedir)
 
 face_cascade = cv2.CascadeClassifier("haarcascade_frontalface_default.xml")
+eye_cascade = cv2.CascadeClassifier("haarcascade_eye.xml")
 
 img=cv2.imread(photo4)
 res_img=cv2.resize(img,(int(img.shape[1]/8),int(img.shape[0]/8)))
-#res_img=cv2.resize(img,(int(img.shape[1]/2),int(img.shape[0]/2)))
 gray_img = cv2.cvtColor(res_img,cv2.COLOR_BGR2GRAY)
 
 faces=face_cascade.detectMultiScale(gray_img,
 scaleFactor=1.05,
 minNeighbors=5)
 
-for x,y,w,h in faces:
-    img = cv2.rectangle(res_img,(x,y),(x+w,y+h),(0,255,0),3)
+for (x,y,w,h) in faces:
+    res_img = cv2.rectangle(res_img,(x,y),(x+w,y+h),(255,0,0),2)
+    
+    roi_gray  = gray_img[y:y+h, x:x+w]
+    roi_color = res_img[y:y+h, x:x+w]
+
+    eyes = eye_cascade.detectMultiScale(roi_gray)
+    for (ex,ey,ew,eh) in eyes:
+        cv2.rectangle(roi_color,(ex,ey),(ex+ew,ey+eh),(0,255,0),2)
 
 print(type(faces))
 print(faces)
 
-cv2.imshow("Face Dection",img)
+cv2.imshow("Face & Eye Detection",res_img)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
